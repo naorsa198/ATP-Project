c2d74c6496a621962c92a422e0ef544bd0c3cacc
naorsa198 <63548776+naorsa198@users.noreply.github.com>
2021-04-05 16:41:53 +0300

    check commit

--- src/algorithms/mazeGenerators/Position.java
+++ src/algorithms/mazeGenerators/Position.java
@@ -3,7 +3,7 @@ package algorithms.mazeGenerators;
 public class Position {
     private int row;
     private int col;
-
+    private string s;
 
     public Position(int row, int col) {
         this.row = row;
